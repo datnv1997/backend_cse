0cf6edbd6f1f31f22b1f8385f6d975375cd0cf19
Nguyen Dat <datnv1997@gmail.com>
2019-10-06 15:37:55 +0700

    update

--- resources/views/backend/categories/categories.blade.php
+++ resources/views/backend/categories/categories.blade.php
@@ -2,44 +2,121 @@
 @extends('backend.layouts.master')
 
 <!-- Page title -->
-@section('pageTitle') Dashboard @endsection
+@section('pageTitle') Student @endsection
 <!-- End block -->
-@section('extraStyle')
-<style>
-.notification li {
-    font-size: 16px;
-}
 
-.notification li.info span.badge {
-    background: #00c0ef;
-}
+<!-- Page body extra class -->
+@section('bodyCssClass') @endsection
+<!-- End block -->
 
-.notification li.warning span.badge {
-    background: #f39c12;
-}
-
-.notification li.success span.badge {
-    background: #00a65a;
-}
-
-.notification li.error span.badge {
-    background: #dd4b39;
-}
-
-.total_bal {
-    margin-top: 5px;
-    margin-right: 5%;
-}
-</style>
-@endsection
 <!-- BEGIN PAGE CONTENT-->
 @section('pageContent')
+<!-- Section header -->
+<section class="content-header">
+    <h1>
+        Categories
+        <small>List</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{URL::route('user.dashboard')}}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li class="active">Categories</li>
+    </ol>
+</section>
+<!-- ./Section header -->
 <!-- Main content -->
-<h1>CHán</h1>
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-info">
+                <div class="box-header">
+                    @if(AppHelper::getInstituteCategory() == 'college')
+                    <div class="col-md-3">
+                        <div class="form-group has-feedback">
+
+                        </div>
+                    </div>
+                    @endif
+                    <div class="col-md-3">
+                        <div class="form-group has-feedback">
+
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group has-feedback">
+
+                        </div>
+                    </div>
+                    <div class="box-tools pull-right">
+                        <a class="btn btn-info btn-sm" href="{{ URL::route('student.create') }}"><i
+                                class="fa fa-plus-circle"></i> Add New</a>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="table-responsive">
+                        <table id="listDataTableWithSearch"
+                            class="table table-bordered table-striped list_view_table display responsive no-wrap"
+                            width="100%">
+                            <thead>
+                                <tr>
+                                    <th width="5%">#</th>
+                                    <th class="notexport" width="7%">Photo</th>
+                                    <th width="8%">Regi. No.</th>
+                                    <th width="8%">Roll No.</th>
+                                    <th width="8%">ID Card</th>
+                                    <th width="19%">Name</th>
+                                    <th width="10%">Phone No</th>
+                                    <th width="10%">Email</th>
+                                    <th width="10%">Status</th>
+                                    <th class="notexport" width="15%">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+
+                                </tr>
+
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th width="5%">#</th>
+                                    <th class="notexport" width="7%">Photo</th>
+                                    <th width="8%">Regi. No.</th>
+                                    <th width="8%">Roll No.</th>
+                                    <th width="8%">ID Card</th>
+                                    <th width="19%">Name</th>
+                                    <th width="10%">Phone No</th>
+                                    <th width="10%">Email</th>
+                                    <th width="10%">Status</th>
+                                    <th class="notexport" width="15%">Action</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.box-body -->
+            </div>
+        </div>
+    </div>
+
+</section>
 <!-- /.content -->
 @endsection
 <!-- END PAGE CONTENT-->
 
 <!-- BEGIN PAGE JS-->
-
+@section('extraScript')
+<script type="text/javascript">
+$(document).ready(function() {
+    window.postUrl = '{{URL::Route("student.status", 0)}}';
+    window.section_list_url = '{{URL::Route("academic.section")}}';
+    window.changeExportColumnIndex = 7;
+    window.excludeFilterComlumns = [0, 1, 8, 9];
+    Academic.studentInit();
+    $('title').text($('title').text() + '-' + $('select[name="class_id"] option[selected]').text() + '(' + $(
+        'select[name="section_id"] option[selected]').text() + ')');
+});
+</script>
+@endsection
 <!-- END PAGE JS-->
